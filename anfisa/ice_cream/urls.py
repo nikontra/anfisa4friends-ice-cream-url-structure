ff199fc951912f8a35f96061ada2dff66560de3a
Третьяков Николай <nikontra@yandex.ru>
2021-12-14 22:50:56 +0300

    edit urls

--- anfisa/ice_cream/urls.py
+++ anfisa/ice_cream/urls.py
@@ -10,8 +10,5 @@ urlpatterns = [
     path('ice_cream/', views.ice_cream_list),
     # Подробная информация о мороженом. Ждем пременную типа int,
     # и будем использовать ее под именем pk
-    path(
-        'ice_cream/<int:pk>/',
-        views.ice_cream_detail
-    ),
+    path('ice_cream/<int:pk>/', views.ice_cream_detail),
 ]
