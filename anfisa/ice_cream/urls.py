6472b0a08982712b29d0235a6f6b815e49c28220
Третьяков Николай <nikontra@yandex.ru>
2021-12-18 08:01:05 +0300

    что то там

--- anfisa/ice_cream/urls.py
+++ anfisa/ice_cream/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # Главная страница
     path('', views.index),
     # Список мороженого
-    path('ice_cream/', views.ice_cream_list),
+    path('ice_cream/', views.ice_cream_list, name = 'ice_cream_list'),
     # Подробная информация о мороженом. Ждем пременную типа int,
     # и будем использовать ее под именем pk
     path('ice_cream/<int:pk>/', views.ice_cream_detail),
